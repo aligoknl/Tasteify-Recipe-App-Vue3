2a89620b83573d3ffc3ff5a727eff84e456293b9
Ali GÖK <97063504+aligoknl@users.noreply.github.com>
2023-09-15 22:52:10 +0200

    added routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SearchingMeal from '../views/SearchingMeal.vue'
+import MealDetails from '../views/MealDetails.vue'
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/by-name/:name?',
+    name: 'searching',
+    component: SearchingMeal
+  },
+  {
+    path: '/meal/:id',
+    name: 'mealDetails',
+    component: MealDetails
+  },
+  {
+    path: '/:pathMatch(.*)',
+    component: NotFound
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
