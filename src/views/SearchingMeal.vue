eb4e5ea542a059eceee0b8f5f8ee965faf042409
Ali GÖK <97063504+aligoknl@users.noreply.github.com>
2023-09-17 16:00:12 +0200

    added search suggestions

--- src/views/SearchingMeal.vue
+++ src/views/SearchingMeal.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="container">
     <div class="bar">
-      <input
-        v-model="searchQuery"
-        @keyup.enter="searchMeals"
-        placeholder="Search for a meal"
-        list="meals"
-        @click="clearInput"
-      />
-      <datalist id="meals">
-        <option v-for="meal in store.searchedMeals" :key="meal.idMeal">{{ meal.strMeal }}</option>
-      </datalist>
-      <AppButton @click="searchMeals" class="search-button">
-        <span class="material-icons">search</span>
-      </AppButton>
+      <div class="input-bar">
+        <input
+          v-model="searchQuery"
+          @keyup.enter="searchMeals"
+          placeholder="Search for a meal"
+          list="meals"
+          @dblclick="clearInput"
+        />
+        <AppButton @click="searchMeals" class="search-button">
+          <span class="material-icons">search</span>
+        </AppButton>
+      </div>
+      <div class="suggestion-box" v-if="showSuggestion">
+        <div
+          v-for="meal in store.searchedMeals"
+          :key="meal.idMeal"
+          class="suggestion-item"
+          @click="selectAndClose(meal)"
+        >
+          {{ meal.strMeal }}
+        </div>
+      </div>
     </div>
     <div v-if="showResults" class="results">
       <MealItem v-if="store.searchedMeals.length > 0" :meal="store.searchedMeals[0]" />
@@ -23,12 +32,12 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { gsap } from 'gsap'
 import { useRecipeStore } from '../stores/recipeStore'
 import MealItem from '../components/MealItem.vue'
 import AppButton from '../components/AppButton.vue'
-
+import { debounce } from '../api/debounce'
 export default {
   components: {
     MealItem,
@@ -38,14 +47,33 @@ export default {
     const store = useRecipeStore()
     const searchQuery = ref('')
     const showResults = ref(false)
+    const showSuggestion = ref(false)
 
     const searchMeals = () => {
       if (searchQuery.value !== '') {
         store.searchMeals(searchQuery.value)
         showResults.value = true
+        showSuggestion.value = true
       } else {
         showResults.value = false
       }
+    }
+
+    const debouncedSearchMeals = debounce(searchMeals, 500)
+
+    watch(searchQuery, () => {
+      debouncedSearchMeals()
+    })
+
+    const selectAndClose = (meal) => {
+      searchQuery.value = meal.strMeal
+      showSuggestion.value = false
+    }
+
+    const selectMeal = (meal) => {
+      searchQuery.value = meal.strMeal
+      showResults.value = false
+      showSuggestion.value = false
     }
 
     const clearInput = () => {
@@ -61,19 +89,6 @@ export default {
       })
     }
 
-    const animateResult = () => {
-      gsap.from('.results', {
-        opacity: 0,
-        y: -50,
-        duration: 3,
-        ease: 'power2.out',
-        onComplete: () => {
-          console.log('hello')
-        }
-      })
-      console.log('sdgsdg')
-    }
-
     onMounted(() => {
       animateSearchBar()
     })
@@ -83,9 +98,13 @@ export default {
       searchQuery,
       searchMeals,
       showResults,
+      showSuggestion,
       clearInput,
       animateSearchBar,
-      animateResult
+      debounce,
+      debouncedSearchMeals,
+      selectMeal,
+      selectAndClose
     }
   }
 }
