1c7c6c459af17311be5acfa63c9435cd1d3fba9e
Ali GÖK <97063504+aligoknl@users.noreply.github.com>
2023-09-18 15:43:22 +0200

    removed spaces

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,10 +2,6 @@
   <main class="container">
     <h1 class="title">4 <span class="material-icons">icecream</span> 4</h1>
     <div class="text">Page Not Found</div>
-    <!-- <router-link to="/" class="text">
-      <span class="material-icons">home</span>
-      <span>Back Home</span>
-    </router-link> -->
     <MealLink :customClass="'text'" :routeName="'home'">
       <span class="material-icons">home</span>
       <span>Back Home</span></MealLink
